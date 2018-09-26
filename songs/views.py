6ffc190bb6327c07ff548877b86f6d48dee16d15
granaderos <marejeanperpinosa@gmail.com>
2018-09-26 10:09:55 +0800

    Genre rendered to index.html

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from . models import Genre
+from . models import Artist
+from . models import Album
+from . models import Song
+from . models import Playlist
 
 def index(request):
-    return render(request, "songs/index.html")
+
+    genres = Genre.objects.all()
+    return render(request, "songs/index.html", context={"genres": genres})
+
