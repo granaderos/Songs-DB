e19223d736383ba46b50c943e4623d2a90b8172d
granaderos <marejeanperpinosa@gmail.com>
2018-09-26 14:04:10 +0800

    Rendering songs based on chosen genre is done.

--- songs/urls.py
+++ songs/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("songs/genre/<int:genre_id>", views.list_songs_based_on_genre, name="genre"),
+    path("search/", views.search_song, name="search_song")
     
 ]
--- songs/views.py
+++ songs/views.py
@@ -10,3 +10,15 @@ def index(request):
     genres = Genre.objects.all()
     return render(request, "songs/index.html", context={"genres": genres})
 
+def list_songs_based_on_genre(request, genre_id):
+    genre = Genre.objects.get(id=genre_id)
+    songs = Song.objects.filter(genre_id=genre_id)
+    return render(request, "songs/genre_songs.html", context={"songs": songs, "genre": genre})
+
+def search_song(request):
+    search_filter = request.GET["search_filter"]
+    search_keyword = request.GET["search_keyword"]
+
+    songs = Song.objects.filter(title__contains=search_keyword)
+
+    return render(request, "songs/search_songs.html", context={"songs": songs, "search_filter": search_filter, "search_keyword": search_keyword})
