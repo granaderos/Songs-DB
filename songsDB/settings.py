626d0aeb10117d68a5b2989c5d53526578a960b6
Marejean Perpinosa <marejeanperpinosa@gmail.com>
2018-11-29 15:32:46 +0800

    Update settings.py

--- songsDB/settings.py
+++ songsDB/settings.py
@@ -184,7 +184,5 @@ MEDIA_URL = "/media/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'marejeanperpinosa@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ffnmarjiecasosamarjiecasosag'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'marejeanperpinosa@gmail.com'
